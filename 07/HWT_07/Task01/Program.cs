353c90ee9b58594e57e5e423c53c255feea95a43
anchurakov <ownedblackhood@mail.ru>
2017-06-24 03:00:49 +0300

    Выполнил только первое задание, на других блы ступор.

---
+++ 07/HWT_07/Task01/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01
+{
+    class Program
+    {
+        static void PrintList(IEnumerable<int> list)
+        {
+            foreach (var item in list)
+                Console.Write("{0} ", item);
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            var list = new List<int>();
+
+            Console.WriteLine("Введите число n");
+
+            int n = int.Parse(Console.ReadLine());
+
+            for(int j = 1; j <= n; j++)
+            {
+                list.Add(j);
+            }
+
+            PrintList(list);
+            bool del = false;
+
+            while (list.Count > 1)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (del)
+                    {
+                        list.RemoveAt(i--);
+                    }
+                    del = !del;
+                }
+                PrintList(list);
+            }
+            Console.Read();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 07/HWT_07/Task02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String str = "";
+
+            int i;          
+
+            Console.WriteLine("Введите текст на английском");
+
+            str = Console.ReadLine();
+
+            str = str.ToLower();
+
+            string[] split = str.Split(' ', ',');
+
+            List<string> list = new List<string>();
+
+            list.Add(str);
+
+            foreach(var s in list)
+            {
+                if(s.Contains(s))
+                {
+                    list.Add()
+                }
+            }
+         
+            Console.ReadKey();
+        }
+    }
+}
